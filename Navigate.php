34e3d429d599d87f650e22f0af69c166e6928b84
Kristopher Weinert <k.weinert98@gmail.com>
2015-01-03 22:50:57 -0500

    Test
    This creates finds the local hard drive and then puts out how much
    storage there is left and rounds it. This also determines if the
    computer is running windows or mac.

---
+++ Navigate.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+	<title>Navigate</title>
+	<style>
+	</style>
+	</head>
+	<body>
+		<?php
+			$ds = disk_free_space("/");
+			$cool = $ds / 1000000000;
+			if ($ds == false){
+				$win = disk_free_space(":C");
+				$wincool = $win / 1000000000;
+				echo '<center><b>'.(round($wincool)).'</b></center>';
+			}else{
+				echo '<center><b>'.(round($cool)).'</b></center>';
+			}
+		?>
+	</body>
+</html>
---
+++ README.md
@@ -0,0 +1,3 @@
+Navigate
+========
+This program is desgined to acces your hard drive anywhere.
